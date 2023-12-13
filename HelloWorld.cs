cddbf2407f028007d5600b708d29e2625d144ab1
gosha4323 <153745851+gosha4323@users.noreply.github.com>
2023-12-13 17:29:56 +0300

    Add files via upload
    Script

---
+++ HelloWorld.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hello world");
+        print(12);
+        print("hello world"+25);
+        
+       
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("text");
+        
+    }
+}
